87e6c9845f9fd373a915d625e762564efcb0b93c
Josh Rudge <joshrudge@hotmail.com>
2024-06-17 14:47:27 +0000

    added the introduction to the game, user can now enter their name and nationality. Once the user has entered this the data is then added to the introduction

--- run.py
+++ run.py
@@ -24,3 +24,29 @@ ascii_art = """
   ███          ███    ███ ███   ███    ▄█    ███
  ▄████▀        ██████████  ▀█   █▀   ▄████████▀
 """
+
+def main():
+    print_one_letter_at_a_time(ascii_art)
+    if get_yes_no_input("Can you step up? (yes/no): \n") == 'yes':
+        try:
+            name = input("Please enter your name: \n").strip()
+            while not name:
+                print("Name cannot be empty. Please enter a valid name.")
+                name = input("Please enter your name: \n").strip()
+
+            nationality = input(f"Please can {name} enter the country they represent: \n").strip()
+            while not nationality:
+                print("Country cannot be empty. Please enter a valid country.")
+                nationality = input(f"Please can {name} enter the country they represent: \n").strip()
+
+            print(f"We have {name} from {nationality}\n")
+            print("""
+            Aim is to pick a number different to the computer in order to score.
+            If you and the computer both have the same number, the computer makes the save.
+            If you and the computer both pick different numbers, then you score.
+            Example: if you type 2 and the computer types 3, you've scored a goal. 
+            However, if you type 1 and the computer types 1, then the computer makes the save.
+            You can go 1 = top left, 2 = bottom left, 3 = middle, 4 = top right, and 5 = bottom right.
+            Please don't let your country down. Don't be a Harry Kane!
+            Let's start with {nationality} vs Germany in a penalty shootout!!!
+            """)
